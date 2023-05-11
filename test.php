3507d3c37f65c9365e56b7a148f4c6d7cf1157c6
Aurel2003 <131757068+Aurel2003@users.noreply.github.com>
2023-05-11 11:14:14 +0100

    da

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
-      echo "<table border='1'><tr><th>Nombre</th><th>Descripción</th></tr>";
+      echo "<table border='1'><tr><th>Nombre</th><th>Descripción</th><th>Descripción</th><th>URL con más información</th><th>URL de Google Maps</th><th>Imagen</th>";
        $myfile = fopen("textfile.csv","r") or die("Unable to open file!");
        while(!feof($myfile)){
            $line = fgets(($myfile));
 
            $array_line = explode(";",$line);
-           echo "<tr><td>".$array_line[0]."</td><td>".$array_line[1]."</td></tr>";
+           echo "<tr><td>".$array_line[0]."</td><td>"."".$array_line[1]."</td><td>".$array_line[2].""."</td><td>".$array_line[3].""."</td><td>".$array_line[4].""."</td><td>".$array_line[5];
        }
       fclose($myfile);
 
